522d89c409ae8950a6862b6efacbc5ed8852f194
MartinMraovic <Martin.Mraovic@students.ittralee.ie>
2023-04-26 23:40:21 +0000

    Using recursive function
    Signed-off-by: MartinMraovic <Martin.Mraovic@students.ittralee.ie>

--- main.cpp
+++ main.cpp
@@ -1,38 +1,36 @@
 #include "mbed.h"
 
-// Declares variables for nth term, previous two terms, and the next term
+// set value of nth?term to 9 
 int nth_term = 9;
-int term_1, term_2;
-int next_term = 0;
 
+// Declare function which will calculate nth fibb term
+int fib(int n);
+
+// Main function
 int main() {
 
-    // Print message Lab 2 program to indicate start of the program
+// Print initial message to terminal 
     printf("Lab 2 program \r\n");
 
-    // Initialize term_1 and term_2 to 0 and 1
-    term_1 = 0;
-    term_2 = 1;
+// Loop trough first nth terms of fibb sequence
+    for(int i=0; i<nth_term; i++){
 
-    // Print the value of nth_term and the first two terms of the sequence
-    printf("nth term is %d \r\n", nth_term);
-    
-    printf("0, 1, ");
+        // Calculate and print term
+        printf("%d, ", fib(i));
+    }
+    // Print meesege to indicate end of sewuence
+    printf("..!\r\n");
+}
 
-    // Loop through the rest of terms of the sequence
-    for(int i=2; i<nth_term; i++){
-
-        // Compute the next term in the sequence
-        next_term = term_1 + term_2;
-
-        // Print the next term 
-        printf("%d, ", next_term);
-
-        // Update previous two terms for the next iteration of the loop
-        term_1 = term_2;
-        term_2 = next_term;
+// Fibb function
+int fib(int n){
+    // take an input, calculate part of fibb and return result
+    if(n == 0) // 0th fibb number is 0
+        return 0;
+    if(n == 1) // 1st fibb number is 1
+        return 1;
+    else{ // for all other values of n, use recursion to calculate nth fibb number
+        return (fib(n-1) + fib(n-2));
     }
 
-    // Print a message to indicate the end of the sequence
-    printf("..!\r\n");
 }
